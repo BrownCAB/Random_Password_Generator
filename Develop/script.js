47eae8511da26efb5d40be75eb1947dfee1f5701
BrownCAB <brown.calah@hotmail.com>
2022-08-29 18:30:38 -0700

    statered modifying the script code

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,30 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Assignment code here
+function generatePassword() {
+ 
+  var userInput = window.prompt("Hey! you clicked the Button!")
+
+  console.log(userInput)
+}
+
+
+
+// 1 Prompt the user for the password criteria
+
+// a Password Length 8-128 chararters
+// var length = Number(prompt("Enter a password length between 8 and 128")),
+// b lowercase, uppercase, numeric, and/or special characters
+//charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
+
+// 2 validate the input
+// 3 genetate password
+//password = generatePassword();
+
+
+// 4 display genetated password on the page
+ // return "Generated password will go here!";
 
 // Write password to the #password input
 function writePassword() {
