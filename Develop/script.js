40ffbb8104d6c5911f0146a5c4eba6f34e2572d1
BrownCAB <brown.calah@hotmail.com>
2022-08-31 22:16:54 -0700

    fixed and modified script code

--- Develop/script.js
+++ Develop/script.js
@@ -1,90 +1,100 @@
+// Assignment code here
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Assignment code here
+function randomInt(max, min) {
+  if (!max) {
+  max - min
+  min - 0
+  }
+ var rand = Math.random();
+  return Math.floor(min*(1 - rand) + rand*max); 
+}
+
+function getRandomItem(list) {
+  var randomIndex = Math.floor(Math.random()*list.length)
+    return list[randomIndex]
+}
+
 function generatePassword() {
-// How many characters would like between 8 to 128?
-var userInput = window.prompt("How many characters would you like for your password?");
-var passwordLength = parseInt(userInput);
 
-if (Number.isNaN(passwordLength)) {
-  window.alert("Please input a number.");
-  return
-}
-
-if (passwordLength < 8 || passwordLength > 128) {
-  window.alert("Please select a number between 8 to 128 characters.");
-  return
-}
-
-// prompt user to confirm if they want lower case characters
-var userSelectsLowercase = window.confirm("Would you like to include lowercase characters to your password?");
-// prompt user to confirm if they want upper case characters
-var userSelectsUppercase = window.confirm("Would you like to include uppercase characters to your password?");
-//prompt user to confirm if they want numerical characters
-var userSelectsNumbers = window.confirm("Would like to include numbers to your password?");
-// prompt user to confirm if they want special characters
-var userSelectsSpecial = window.confirm("Would you like to include special characters to your password?");
 // prompt user to validate atleast one prompt
 
-// variables list
-var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var userInput = window.prompt("How long do you want your password to be?");
+
+  var passwordLength = parseInt(userInput);
+ 
+
+ if (userInput >= 8) {
+  window.confirm("Please confirm " + userInput + " is correct!")
+  } 
+  
+if (isNaN(passwordLength)) {
+  window.alert("That's not a numder!"); 
+  return
+  } 
+  
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Invalid! Please input a number between 8 and 128 characters.");
+  return
+  }
+
+ //prompt user to confirm if they want numerical characters
+var userWantsNumbers = window.confirm("Would you like to include numbers to your password?");
+// prompt user to confirm if they want special characters
+var userWantsSymbols = window.confirm("Would you like to include special characters to your password?");
+ // prompt user to confirm if they want lower case characters
+var userWantsLowercase = window.confirm("Would you like to include lowercase characters to your password?");
+// prompt user to confirm if they want upper case characters
+var userWantsUppercase = window.confirm("Would you like to include uppercase characters to your password?");
+
 var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialList = ["|", "!", "#", "$", "%", "^", "&", "*", "_", "-"];
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // if statements
-var optionsList = []
-console.log(optionsList)
+var optionsList = [];
 
-if (userSelectsLowercase === true) {
-  optionsList.push(lowercaseList);
-} 
-
-if (userSelectsUppercase === true) {
-  optionsList.push(uppercaseList);
-}
-
-if (userSelectsNumbers === true) {
+if (userWantsNumbers === true) {
   optionsList.push(numbersList);
 }
 
-if (userSelectsSpecial === true) {
+if (userWantsSymbols === true) {
   optionsList.push(specialList);
 }
-// Make sure that aleast one character of each array is picked
 
+if (userWantsLowercase === true) {
+  optionsList.push(lowercaseList);
+} 
 
-// Generate Password for the User
+if (userWantsUppercase === true) {
+  optionsList.push(uppercaseList);
+}
+
+// Generate Password 
 var generatedPassword = ""
 
+// Make sure that atleast one character of each array is picked
 for (var i = 0; i < passwordLength; i++) {
   var randomList = getRandomItem(optionsList);
   var randomChar = getRandomItem(randomList);
-  console.log(randomList)
-  console.log(randomChar)
   generatedPassword += randomChar
-  console.log(generatedPassword)
-}
- return generatedPassword
 }
 
-function getRandomItem(someArray) {
-  var randomIndex = Math.floor(Math.random()*someArray.length)
-  console.log("randomIndex", randomIndex)
-  return someArray[randomIndex]
+return generatedPassword
+
 }
 
-// Write password to the #password Input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
